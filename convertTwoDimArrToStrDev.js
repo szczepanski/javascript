2fb588273e5dc260b4c6a5b3babbef060f5f7151
Piotr <ask.piotr@yahoo.co.uk>
2018-03-10 20:16:41 +0100

    Delete convertTwoDimArrToStrDev.js

--- convertTwoDimArrToStrDev.js
+++
@@ -1,9 +0,0 @@
-function twoDimArrToStr(two_d_arr,separator_l1,separator_l2) {
-     var tmp = "";
-     // bypass last element to avoid adding separator_l2 at the end of return tmp output
-     for (i=0; i< two_d_arr.length-1; i++){
-          tmp = tmp + arrToStr(two_d_arr[i],separator_l1) + separator_l2;
-          }
-        //add last element without separator   
-        tmp = tmp + arrToStr(two_d_arr[i],separator_l1);
-     return tmp;}
